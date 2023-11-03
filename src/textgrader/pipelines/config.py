d8369206d4196b29c0b56a6bde92e82d4f561832
= <=>
2023-11-03 14:28:23 -0300

    primeiro commit

---
+++ src/textgrader/pipelines/config.py
@@ -0,0 +1,27 @@
+
+TARGETS_1 = ['Domínio da modalidade escrita formal_nota',                                                                                                  
+ 'Compreender a proposta e aplicar conceitos das várias áreas de conhecimento para desenvolver o texto dissertativo-argumentativo em prosa_nota',   
+ 'Selecionar, relacionar, organizar e interpretar informações em defesa de um ponto de vista_nota',                                                 
+'Conhecimento dos mecanismos linguísticos necessários para a construção da argumentação_nota',                                                     
+'Proposta de intervenção com respeito aos direitos humanos_nota']           
+
+
+TARGETS_3 = ['Conteúdo_nota',                                                                                                                               
+'Estrutura do texto_nota',                                                                                                               
+'Estrutura de ideias_nota',                                                                                                                 
+'Vocabulário_nota',                                                                                                                       
+'Gramática e ortografia_nota'] 
+
+TARGETS_2 =  ['Adequação ao Tema_nota',                                                                                                                        
+ 'Adequação e Leitura Crítica da Coletânea_nota',                                                                                                
+ 'Adequação ao Gênero Textual_nota',                                                                                                               
+ 'Adequação à modalidade padrão da língua_nota',                                                                                                    
+ 'Coesão e Coerência_nota']
+
+ID_VARS = ['index','tema','conjunto']
+
+ALL_TARGETS = TARGETS_1 + TARGETS_2 + TARGETS_3 
+
+EXCLUDE_COLS = ALL_TARGETS + ID_VARS
+ 
+                                 
